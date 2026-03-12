a98e0b6431a3949e34aae06b79f49fe8d5def730
Marcin Śliwiński <kwadrat@autograf.pl>
2026-03-12 15:51:14 +0100

    Add search and filters to Plants TableView

--- src/main/java/com/egen/fitogen/ui/controller/PlantsController.java
+++ src/main/java/com/egen/fitogen/ui/controller/PlantsController.java
@@ -1,13 +1,20 @@
 package com.egen.fitogen.ui.controller;
 
 import com.egen.fitogen.domain.Plant;
-import com.egen.fitogen.repository.PlantRepository;
 import com.egen.fitogen.database.SqlitePlantRepository;
+import com.egen.fitogen.repository.PlantRepository;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+
 import javafx.fxml.FXML;
+
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PlantsController {
@@ -30,8 +37,13 @@ public class PlantsController {
     @FXML
     private TableColumn<Plant, String> colLatin;
 
+    @FXML
+    private TextField searchField;
+
     private final PlantRepository plantRepository =
             new SqlitePlantRepository();
+
+    private ObservableList<Plant> masterData;
 
     @FXML
     public void initialize() {
@@ -56,16 +68,46 @@ public class PlantsController {
                 new PropertyValueFactory<>("latinSpeciesName")
         );
 
-        refresh();
-    }
-
-    private void refresh() {
-
-        table.setItems(
-                FXCollections.observableArrayList(
-                        plantRepository.findAll()
-                )
+        masterData = FXCollections.observableArrayList(
+                plantRepository.findAll()
         );
+
+        FilteredList<Plant> filteredData =
+                new FilteredList<>(masterData, p -> true);
+
+        searchField.textProperty().addListener((obs, oldVal, newVal) -> {
+
+            filteredData.setPredicate(plant -> {
+
+                if (newVal == null || newVal.isEmpty()) {
+                    return true;
+                }
+
+                String keyword = newVal.toLowerCase();
+
+                if (plant.getSpecies().toLowerCase().contains(keyword))
+                    return true;
+
+                if (plant.getVariety() != null &&
+                        plant.getVariety().toLowerCase().contains(keyword))
+                    return true;
+
+                if (plant.getLatinSpeciesName() != null &&
+                        plant.getLatinSpeciesName().toLowerCase().contains(keyword))
+                    return true;
+
+                return false;
+            });
+
+        });
+
+        SortedList<Plant> sortedData =
+                new SortedList<>(filteredData);
+
+        sortedData.comparatorProperty()
+                .bind(table.comparatorProperty());
+
+        table.setItems(sortedData);
 
     }
 
